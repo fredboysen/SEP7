3d325e358adc08f92e736099d1b705db3f32c5ef
Thomas Schelde <Thomas_Schelde@hotmail.com>
2024-12-08 16:36:06 +0100

    login

--- SEP7.WebApp/Program.cs
+++ SEP7.WebApp/Program.cs
@@ -11,32 +11,8 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7073/") });
 
-<<<<<<< HEAD
 builder.Services.AddScoped<IAuthService, AuthService>();
 
-=======
-// Add MudBlazor services
-builder.Services.AddMudServices(); 
-
-// Add HttpClient for API interaction
-builder.Services.AddHttpClient("WebAPI", client => { 
-    client.BaseAddress = new Uri("https://localhost:7073");
-});
-
-builder.Services.AddHttpClient();
-
-// Add CORS policy
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowBlazorApp", policy =>
-    {
-        policy.WithOrigins("https://localhost:5123")  
-              .AllowAnyMethod()
-              .AllowAnyHeader()
-              .AllowCredentials();
-    });
-});
->>>>>>> 1d7e64557afd271f9e2aea451f9e0cb30882e578
 
 var app = builder.Build();
 
@@ -47,17 +23,9 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-<<<<<<< HEAD
 app.UseCors("AllowAll"); // Apply the CORS policy
 app.UseHttpsRedirection();
 app.UseAntiforgery(); // Ensure it does not block your upload requests
-=======
-
-
-
-app.UseHttpsRedirection();
-app.UseAntiforgery();
->>>>>>> 142c714d59f339eebfb3a4691ec7ab0010e1e622
 app.UseStaticFiles();
 app.MapStaticAssets();
 app.MapRazorComponents<SEP7.WebApp.Components.App>()
